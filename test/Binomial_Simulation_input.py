c2c9edd0134f73eaa4b68d56679cd9661fe51609
JingShing <JingShingLai@gmail.com>
2023-01-09 00:24:26 +0800

    Update Binomial_Simulation_input.py

--- test/Binomial_Simulation_input.py
+++ test/Binomial_Simulation_input.py
@@ -2,6 +2,7 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import binom
+
 n = int(input("Please enter n(int): "))
 p = float(input("Please enter p(float): "))
 size = int(input("Please enter size(int): "))
@@ -11,4 +12,7 @@ print("Mean:", mean)
 print("Probability of "+str(target)+":", binom.pmf(target, n, p)*100)
 sns.displot(random.binomial(n=n, p=p, size=size))
 plt.title("Binomial Simulation w/ n = "+str(n)+", p = "+str(p))
-plt.show() 
+plt.tight_layout()
+plt.savefig('plot.png',dpi=300)
+plt.show()
+plt.close('all')
